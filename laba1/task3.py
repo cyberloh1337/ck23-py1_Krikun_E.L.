8f308fa8b3b06865495dddda5e0006fa1d6b5cce
cyberloh1337 <153020576+cyberloh1337@users.noreply.github.com>
2023-12-05 22:39:35 +0300

    Add files via upload

---
+++ laba1/task3.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+team1 = list_players[:3]
+team2 = list_players[3:]
+print(team1)
+print(team2)
+# TODO Разделите участников на две команды
