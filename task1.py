83caa418ecdc8c7261586f27759ae2b1d6d4d77f
cyberloh1337 <153020576+cyberloh1337@users.noreply.github.com>
2024-01-14 05:07:03 +0300

    Лабораторная работа 3

---
+++ task1.py
@@ -0,0 +1,15 @@
+# TODO Напишите функцию для поиска индекса товара
+def lin_search(a, n):
+    for i in range(len(a)):
+        if a[i] == n:
+            return i
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = lin_search(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
